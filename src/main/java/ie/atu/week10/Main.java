1a14d9dd7af0f7b1176967ed41555ac20ca2a8ca
Patrick <g00409592@atu.ie>
2023-01-09 18:29:26 +0000

    Created ParentClass and renamed main to ParentChildDemo.

--- src/main/java/ie/atu/week10/Main.java
+++
@@ -1,7 +0,0 @@
-package ie.atu.week10;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/ie/atu/week10/ParentClass.java
@@ -0,0 +1,11 @@
+package ie.atu.week10;
+
+public class ParentClass {
+    public ParentClass() {
+        System.out.println("You are calling the parent constructor");
+    }
+
+    public ParentClass(String message) {
+        System.out.println(message);
+    }
+}
